bdee9a6536876147894f3dd297205489ea610c68
Haruka31Dev <u202412824@upc.edu.pe>
2024-05-27 11:59:40 -0500

    owo

--- src/services/send-cotization.js
+++ src/services/send-cotization.js
@@ -16,7 +16,9 @@ export const sendCotization = async (data) => {
 }
 export const getClientDataByDNIID = async (data) => {
     try {
-        const response = await axios.post(import.meta.env.VITE_API_URL+'get-client-data', data);
+        const response = await axios.post(import.meta.env.VITE_API_URL+'get-client-data', data,{
+            headers: { 'Content-Type': 'application/json','api_key': import.meta.env.VITE_API_KEY}
+        });
         return response.data;
     } catch (error) {
         console.error('Error getting client data:', error);
